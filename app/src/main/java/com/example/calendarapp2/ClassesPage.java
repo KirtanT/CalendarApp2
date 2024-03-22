39d3536574c4ca266d86565f4417f8aac466dd3f
spencer-banko <spencerbanko@gmail.com>
2024-03-21 23:49:35 -0400

    Made Full Front End
    Did front end for all pages we should need. Needs functionality

---
+++ app/src/main/java/com/example/calendarapp2/ClassesPage.java
@@ -0,0 +1,14 @@
+package com.example.calendarapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classes_page);
+    }
+}
---
+++ app/src/main/java/com/example/calendarapp2/OtherPage.java
@@ -0,0 +1,14 @@
+package com.example.calendarapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtherPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_page);
+    }
+}
