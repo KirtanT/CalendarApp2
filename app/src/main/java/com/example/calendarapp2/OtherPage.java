26e59fdd6f8221e16e8653ac40d1d874f9d1bd16
spencer-banko <spencerbanko@gmail.com>
2024-03-23 12:33:46 -0400

    Fixed Constraints, started creating events/arraylist and adding functionality

--- app/src/main/java/com/example/calendarapp2/OtherPage.java
+++ app/src/main/java/com/example/calendarapp2/OtherPage.java
@@ -1,14 +1,30 @@
 package com.example.calendarapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 
 public class OtherPage extends AppCompatActivity {
 
+    private RecyclerView calendarRecyclerView;
+    private ListView OtherListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_page);
+        OtherListView = findViewById(R.id.OtherListView);
+
     }
 }
