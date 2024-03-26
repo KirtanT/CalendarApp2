f6024268a81ac3c84bfb472465ff5e5164ef6dfa
Kirtan Thakkar <kirtanhthakkar@gmail.com>
2024-03-26 13:19:24 -0400

    added edit and delete functionality

--- app/src/main/java/com/example/calendarapp2/EventAdapter.java
+++ app/src/main/java/com/example/calendarapp2/EventAdapter.java
@@ -1,6 +1,7 @@
 package com.example.calendarapp2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +19,13 @@ import java.util.List;
 public class EventAdapter extends ArrayAdapter<Event> {
 
     private Context context;
+    private ArrayList<Event> events;
+
 
     public EventAdapter(Context context, ArrayList<Event> events) {
         super(context, 0, events); // should it be R.layout.event_cell?
         this.context = context;
+        this.events = events;
     }
 
     @Override
@@ -34,6 +38,9 @@ public class EventAdapter extends ArrayAdapter<Event> {
         TextView timeTextView = eventView.findViewById(R.id.SecondEventCell);
         TextView dueDateTextView = eventView.findViewById(R.id.ThirdEventCell);
 
+        Button deleteButton = eventView.findViewById(R.id.deleteButton);
+        Button editButton = eventView.findViewById(R.id.editButton);
+
         Event event = getItem(position);
 
         categoryTextView.setText(event.getCategory());
@@ -41,6 +48,51 @@ public class EventAdapter extends ArrayAdapter<Event> {
         timeTextView.setText(event.getTime());
         dueDateTextView.setText(event.getDueDate());
 
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                events.remove(event);
+                notifyDataSetChanged();
+            }
+        });
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getItem(position).getCategory().equals("Class")){
+                    events.remove(event);
+                    Intent x = new Intent(getContext().getApplicationContext(), NewClass.class);
+                    x.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    getContext().startActivity(x);
+                    notifyDataSetChanged();
+                }
+                if (getItem(position).getCategory().equals("Assignment")){
+                    events.remove(event);
+                    Intent x = new Intent(getContext().getApplicationContext(), NewAssignment.class);
+                    x.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    getContext().startActivity(x);
+                    notifyDataSetChanged();
+                }
+                if (getItem(position).getCategory().equals("Exam")){
+                    events.remove(event);
+                    Intent x = new Intent(getContext().getApplicationContext(), NewExam.class);
+                    x.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    getContext().startActivity(x);
+                    notifyDataSetChanged();
+                }
+                else {
+                    events.remove(event);
+                    Intent x = new Intent(getContext().getApplicationContext(), NewToDo.class);
+                    x.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    getContext().startActivity(x);
+                    notifyDataSetChanged();
+                }
+
+            }
+        });
+
+        
         return eventView;
     }
 }
